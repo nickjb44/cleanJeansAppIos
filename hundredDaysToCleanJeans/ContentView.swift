e54aa55c67bb4857383b0edd1b6a8fb5cc0cc8d6
Nick Buser <nick.buser@earnin.com>
2024-07-13 12:50:55 -0700

    Initial Commit

---
+++ hundredDaysToCleanJeans/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hundredDaysToCleanJeans
+//
+//  Created by Nick Buser on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hundredDaysToCleanJeans/hundredDaysToCleanJeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  hundredDaysToCleanJeansApp.swift
+//  hundredDaysToCleanJeans
+//
+//  Created by Nick Buser on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct hundredDaysToCleanJeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
